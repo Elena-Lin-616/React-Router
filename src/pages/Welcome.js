517401963cc3681bbaaa4aa9783c204565fa1916
Na Lin <lna483018@gmail.com>
2022-12-04 00:48:35 -0500

    nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,13 @@
+import { Route } from "react-router-dom";
 function Welcome() {
-  return <div>welcome</div>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome new user</p>
+      </Route>
+    </section>
+  );
 }
 
 export default Welcome;
