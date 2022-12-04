9d0a449ef363190b469644eeed3d3cb5cbf82de8
Na Lin <lna483018@gmail.com>
2022-12-04 00:10:56 -0500

    V5: Link to navigate around app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Welcome, Products } from "./pages";
+import { MainHeader } from "./components";
 import { Route } from "react-router-dom";
 function App() {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
