c4da5fa5432591a41b9e429d08e6f8c62560ff36
Na Lin <lna483018@gmail.com>
2022-12-04 00:34:01 -0500

    extra route params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
 function ProductDetail() {
+  const params = useParams(); // return a params object -> {productId: value}( dynamic route )
+
   return (
     <section>
       <h1>Product detail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 }
